f614d9efd5905e4b78647bd12ff51a3eb28fe852
c-chemwa <calebchemwa@gmail.com>
2023-06-16 13:17:06 +0300

    login and sign up forms

---
+++ login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Log In</title>
+    <link rel="stylesheet" href="./signup.css">
+    <style>
+        .form{
+            width: 230px;
+            height: 280px;
+        }
+    </style>
+</head>
+<body>
+    <div class="form">
+        <div class="title">
+            <h1>Log In</h1>
+        </div>
+        <form action="" method="post">
+            <input type="text" name="ID" placeholder="Your ID">
+            <input type="password" name="Password" placeholder="Password">
+            <input type="submit" value="Log In" name="Login Button">
+        </form>
+    </div>
+</body>
+</html>
